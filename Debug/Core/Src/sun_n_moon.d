240d6c3d9aa82073cdf400f71bdea880dfba394a
Vitaliy Bunin <v.p.bunin@gmail.com>
2020-07-27 18:49:07 +0300

    Debug objects are deleted.

--- Debug/Core/Src/sun_n_moon.d
+++
@@ -1,3 +0,0 @@
-Core/Src/sun_n_moon.o: ../Core/Src/sun_n_moon.c ../Core/Inc/sun_n_moon.h
-
-../Core/Inc/sun_n_moon.h:
--- Debug/Core/Src/ws2812.d
+++
@@ -1,3 +0,0 @@
-Core/Src/ws2812.o: ../Core/Src/ws2812.c ../Core/Inc/ws2812.h
-
-../Core/Inc/ws2812.h:
